e52b42eae6064ff8bfdec786115c4620bac4258b
Ruben Mota <rubenlisboa95@gmail.com>
2023-06-12 16:20:39 +0100

    added a controller actions, routes and view files

--- app/controllers/incomes_controller.rb
+++ app/controllers/incomes_controller.rb
@@ -1,2 +1,46 @@
 class IncomesController < ApplicationController
+
+  def index
+    @incomes = Income.all
+  end
+
+  def show
+    @income = Income.find(params[:id])
+  end
+
+  def new
+    @income = Income.new
+  end
+
+  def create
+    @income = Income.new(income_params)
+    @income.user = current_user
+    if @income.save
+      redirect_to incomes_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @income = Income.find(params[:id])
+  end
+
+  def update
+    @income = Income.find(params[:id])
+    @income.update(income_params)
+    redirect_to incomes_path
+  end
+
+  def destroy
+    @income = Income.find(params[:id])
+    @income.destroy
+    redirect_to incomes_path
+  end
+
+  private
+
+  def income_params
+    params.require(:income).permit(:amount, :category, :name, :recurring)
+  end
 end
