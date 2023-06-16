268454d759ae93fc2a55163bac077eeaf377db76
Ruben Mota <rubenlisboa95@gmail.com>
2023-06-16 14:44:30 +0100

    fixed

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -10,5 +10,5 @@ application.register("hello", HelloController)
 import TomSelectController from "./tom_select_controller"
 application.register("tom-select", TomSelectController)
 
-import TooltipController from "./tooltip_controller"
-application.register("tooltip", TooltipController)
+// import TooltipController from "./tooltip_controller"
+// application.register("tooltip", TooltipController)
