ecebce3bd35d82f693da38a8c5305f9cae49de53
israel chioke <chiokeemirate@gmail.com>
2023-06-20 19:14:46 +0400

    styled login form

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema[7.0].define(version: 2023_06_20_103113) do
-=======
-ActiveRecord::Schema[7.0].define(version: 2023_06_19_133236) do
->>>>>>> master
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -122,8 +118,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_19_133236) do
     t.datetime "updated_at", null: false
     t.string "first_name"
     t.string "last_name"
-    t.string "username"
     t.float "balance", default: 0.0, null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
