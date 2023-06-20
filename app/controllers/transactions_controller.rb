54a8d6364ecf921f2050e7772b5b4ea1e16127a1
Ruben Mota <rubenlisboa95@gmail.com>
2023-06-20 10:17:56 +0100

    Aded the diferent graphs

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -3,9 +3,17 @@ class TransactionsController < ApplicationController
   def index
     @transactions = Transaction.where(user: current_user, group_id: nil)
     if params[:query].present?
-      @transactions =Transaction.search_by_name_kind_category_amount_frequency(params[:query])
+      @transactions = Transaction.search_by_name_kind_category_amount_frequency(params[:query])
+      if params[:query] == "expense" || params[:query] == "income"
+        @donut_chart_data = Transaction.where(user: current_user, group_id: nil, kind: params[:query].capitalize).group(:category).sum(:amount)
+      end
     else
-      @transactions = Transaction.all
+      @transactions = Transaction.where(user: current_user).order(:created_at)
+      balance = 0
+      @area_chart_data = @transactions.map do |transaction|
+        balance += (transaction.kind == 'Expense' ? -1 : 1) * transaction.amount
+        [@transactions.index(transaction), balance]
+      end
     end
   end
 
