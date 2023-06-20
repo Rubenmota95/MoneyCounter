e0775f48a3b722f7ecffdc575b7076a2a0126e1a
israel chioke <chiokeemirate@gmail.com>
2023-06-20 18:58:03 +0400

    add eddit form and username

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,22 @@ class User < ApplicationRecord
   has_many :user_groups, dependent: :destroy
   has_many :groups, through: :user_groups
 
+  validates :username, presence: true, uniqueness: true
+
+
+  private
+
+  def validate_username_uniqueness
+    if username_changed? && User.exists?(username: username)
+      errors.add(:username, 'has already been taken')
+    end
+  end
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+
 end
